dee86132e6258e2dd6decc9f1b061b863efec2bc
renaud marjolaine <mrenaud@amiltone.fr>
2018-06-22 13:33:31 +0200

    second commit

---
+++ app/src/main/java/com/example/mrenaud/recyclertest/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.mrenaud.recyclertest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+
+    private List<MyObject> Species = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        newSpecies();
+
+        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        recyclerView.setAdapter(new MyAdapter(Species));
+    }
+
+    private void newSpecies() {
+        Species.add(new MyObject("Chat", "https://static.wamiz.fr/images/articles/facebook/article/interpreter-attitudes-chat-fb-5942b57e03e2d.jpg"));
+        Species.add(new MyObject("Chien", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR04yehMFOVrI60jFe3GlC8-r7QRCNCzgkasHClyp_54tV8PaPV5Q"));
+        Species.add(new MyObject("Oiseaux", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdCPTy46oJqwcdobjtxDzch4M7_pJCNIxu4JZcC6DtJ9wllRVmYw"));
+        Species.add(new MyObject("Poisson", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRKIKRrNhYGV4O2aBo2w6Tu09oJUKK8Qr9IXqgzv34uUIScTIlTg"));
+        Species.add(new MyObject("Reptile", "http://www.animalzoo.ro/wp-content/uploads/2016/12/soparla-cu-limba-albastra.jpg"));
+        Species.add(new MyObject("Cheval", "http://www.animal-totem.fr/wp-content/uploads/cheval-Animal-Totem-1.jpg"));
+        Species.add(new MyObject("Dinosaure","https://pteapotes.files.wordpress.com/2014/06/capsciences-t-rex-dinosaure-exposition-atelier-animation-bordeaux-enfant-machoire-dent-taille-robot-automate.jpg"));
+
+    }
+
+}
---
+++ app/src/main/java/com/example/mrenaud/recyclertest/MyAdapter.java
@@ -0,0 +1,39 @@
+package com.example.mrenaud.recyclertest;
+
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
+
+    List<MyObject> list;
+
+    public MyAdapter(List<MyObject> list) {
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup viewGroup, int itemType) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.cell_cards, viewGroup, false);
+        return new MyViewHolder(view);
+    }
+
+    public void onBindViewHolder(MyViewHolder myViewHolder, int position) {
+        MyObject myObject = list.get(position);
+        myViewHolder.bind(myObject);
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+
+}
